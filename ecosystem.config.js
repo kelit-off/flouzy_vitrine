dcc2b0b03fcf74bd21f89da18ed5f9788e7f9449
Théo Killian <theo.killian5700@gmail.com>
2025-10-21 15:44:43 +0200

    Changement du port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,13 @@ module.exports = {
     {
       name: "flouzy_vitrine",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 4000",
+      args: "start -p 5000",
       cwd: "./",
       instances: 1, // ou 1 si tu veux juste un process
       exec_mode: "fork",
       env: {
         NODE_ENV: "production",
-        PORT: 4000
+        PORT: 5000
       }
     }
   ]
